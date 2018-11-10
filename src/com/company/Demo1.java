bb76af776cd0363fd8d72afd3f3bb90d47fba682
Will <willvfs@gmail.com>
2018-11-10 03:54:35 -0800

    Finished Worksheet #3

--- src/com/company/Demo1.java
+++ src/com/company/Demo1.java
@@ -3,16 +3,39 @@ package com.company;
 import java.io.*;
 import java.util.Scanner;
 
-public class Demo1 {
-    private Scanner input=null;
+public class Demo1
+{
+    private Scanner input;
+    private int age;
+    private float wage;
+    private String name;
 
-    private Demo1(){
+    private Demo1()
+    {
         input = new Scanner(System.in);
+    }
+
+    private void read()
+    {
+        System.out.print("Enter a name: ");
+        this.name = this.input.nextLine();
+        System.out.print("Enter an age: ");
+        this.age = this.input.nextInt();
+        this.input.nextLine();
+        System.out.print("Enter a wage: ");
+        this.wage = this.input.nextFloat();
+        this.input.nextLine();
+    }
+
+    private void display()
+    {
+        System.out.println("Name: " + name + "\nAge: " + age + "\nWage: $" + wage + "/hour");
     }
 
     public static void main(String[] args)
     {
         Demo1 obj = new Demo1();
-        System.out.println(obj.input.nextLine());
+        obj.read();
+        obj.display();
     }
 }
