171059a49aac25fafe66525507cbdefa9e6daa52
will <willvfs@gmail.com>
2018-11-10 02:00:14 -0800

    basic java program

---
+++ src/com/company/Demo1.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Demo1 {
+    protected Scanner input=null;
+
+    Demo1(){
+        input = new Scanner(System.in);
+    }
+
+    public static void main(String[] args)
+    {
+        Demo1 obj = new Demo1();
+        System.out.println(Demo1.input.nextLine());
+    }
+}
